a479bf8519f6b354d6970b01510ecc09640f2240
Francisco Maycon <maycondeveloper09@gmail.com>
2024-07-05 06:21:05 -0300

    Configuração do arquivo models.py

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Contact(models.Model):
+    Primeiro_nome = models.CharField(max_length=50)
+    segundo_nome = models.CharField(max_length=50, blank=True)
+    telefone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    Data_atual = models.DateTimeField(default=timezone.now)
+    complemento = models.TextField(blank=True)
