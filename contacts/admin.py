6ecd2a7ad315d97ebc51e865f57d9650d383343f
Francisco Maycon <maycondeveloper09@gmail.com>
2024-07-08 17:44:27 -0300

    Configuração da página admin, listagem de página, campo de pesquisa, entre outros.

--- contacts/admin.py
+++ contacts/admin.py
@@ -3,5 +3,11 @@ from contacts import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    pass
+    list_display = 'Primeiro_nome', 'segundo_nome', 'telefone', 'email', 'Data_atual',
+    ordering = '-id'
+    # list_filter = 'create_date',
+    search_fields = 'Primeiro_nome', 'segundo_nome', 'telefone', 'email',
+    list_per_page = 10
+    list_max_show_all = 200
+    list_display_links = 'Primeiro_nome', 'segundo_nome', 'telefone', 'email',
 
