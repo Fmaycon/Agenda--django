6a39cddf6400ae97f502b639f8675f39d87c27eb
Francisco Maycon <maycondeveloper09@gmail.com>
2024-07-09 12:01:48 -0300

    Rename do segundo_nome --> sobrenome, campos show e pictures adcionados, pasta static criada e adcionada no gitignore e configurações das pastas media_url e media_root nas urls

--- contacts/admin.py
+++ contacts/admin.py
@@ -3,11 +3,11 @@ from contacts import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'Primeiro_nome', 'segundo_nome', 'telefone', 'email', 'Data_atual',
+    list_display = 'Primeiro_nome', 'sobrenome', 'telefone', 'email', 'Data_atual',
     ordering = '-id',
     # list_filter = 'create_date',
-    search_fields = 'Primeiro_nome', 'segundo_nome', 'telefone', 'email',
+    search_fields = 'Primeiro_nome', 'sobrenome', 'telefone', 'email',
     list_per_page = 10
     list_max_show_all = 200
-    list_display_links = 'Primeiro_nome', 'segundo_nome', 'telefone', 'email',
+    list_display_links = 'Primeiro_nome', 'sobrenome', 'telefone', 'email',
 
--- contacts/models.py
+++ contacts/models.py
@@ -3,11 +3,13 @@ from django.utils import timezone
 
 class Contact(models.Model):
     Primeiro_nome = models.CharField(max_length=50)
-    segundo_nome = models.CharField(max_length=50, blank=True)
+    sobrenome = models.CharField(max_length=50, blank=True)
     telefone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
     Data_atual = models.DateTimeField(default=timezone.now)
     complemento = models.TextField(blank=True)
+    show = models.BooleanField(default = True)
+    pictures = models.ImageField(blank = True, upload_to = 'pictures/%Y/%m/')
 
     def __str__(self) -> str:
-        return f'{self.Primeiro_nome} {self.segundo_nome}'
+        return f'{self.Primeiro_nome} {self.sobrenome}'
